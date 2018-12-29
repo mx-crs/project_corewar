ef555f7399c7149fe6b403f2177fd7e2bb090a85
Andriy Gordiychuk <andriy@glenmax.com>
2018-12-29 16:53:34 +0100

    Correct player index printed

--- vm_sources/game1.c
+++ vm_sources/game1.c
@@ -111,7 +111,7 @@ void	ex_live(t_process *pr)
 		{
 			if (pr->vm->visual_mode == 0)
 				ft_printf("A process shows that player %d (%s) is alive\n",
-					tmp->idx, tmp->name);
+					tmp->n, tmp->name);
 			tmp->lives += 1;
 			tmp->last_live = pr->vm->cycle;
 			pr->vm->winner = tmp;
